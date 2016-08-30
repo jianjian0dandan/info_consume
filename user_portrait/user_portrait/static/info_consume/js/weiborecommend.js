a888340bd443f56162d7085aa208aea44699ba3d
linhaobuaa <1257819385@qq.com>
2016-08-30 16:34:38 +0800

    yuyuqi

--- user_portrait/user_portrait/static/info_consume/js/weiborecommend.js
+++ user_portrait/user_portrait/static/info_consume/js/weiborecommend.js
@@ -8,33 +8,77 @@ weiborecommend.prototype=
 	{
 		
 		$.ajax({
-			url:url,
-			type:'GET',
-			dataType:'json',
-			async:true,
-			success:callback,
-			beforeSend:LoadFunction,
-			error:erryFunction
-		});
+		      url: url,
+		      type: 'GET',
+		      dataType: 'json',
+		      async: true,
+		      success:callback
+    	});
 	},
 
 	weibo_hashtag:function(data)
 	{
+		var hashtag=eval(data);
+		console.log(hashtag[0][0]);
+		console.log(hashtag[1][0]);
+		console.log(hashtag[2][0]);
+		console.log(hashtag[3][0]);
+		console.log(hashtag[4][0]);
+		console.log(hashtag[5][0]);
+		console.log(hashtag[6][0]);
+		console.log(hashtag[7][0]);
+		console.log(hashtag[8][0]);
+		console.log(hashtag[9][0]);
+		console.log(hashtag[10][0]);
+		console.log(hashtag[11][0]);
+		console.log(hashtag[12][0]);
+		console.log(hashtag[13][0]);
+		console.log(hashtag[14][0]);
+		console.log(hashtag[15][0]);
+		console.log(hashtag[16][0]);
+		console.log(hashtag[17][0]);
+		console.log(hashtag[18][0]);
+		console.log(hashtag[19][0]);
 
-	}
+		//限制打印20个hashtag
+		$('#hashone').append(hashtag[0][0]);				
+		$('#hashtwo').append(hashtag[1][0]);
+		$('#hashthree').append(hashtag[2][0]);			
+		$('#hashfour').append(hashtag[3][0]);
+		$('#hashfive').append(hashtag[4][0]);
+		$('#hashsix').append(hashtag[5][0]);
+		$('#hashseven').append(hashtag[6][0]);
+		$('#hasheight').append(hashtag[7][0]);
+		$('#hashnine').append(hashtag[8][0]);
+		$('#hashten').append(hashtag[9][0]);
+		$('#hasheleven').append(hashtag[10][0]);
+		$('#hashtwlve').append(hashtag[11][0]);
+		$('#hashthirteen').append(hashtag[12][0]);
+		$('#hashfourteen').append(hashtag[13][0]);
+		$('#hashfifteen').append(hashtag[14][0]);
+		$('#hashsixteen').append(hashtag[15][0]);
+		$('#hashseventeen').append(hashtag[16][0]);
+		$('#hasheighteen').append(hashtag[17][0]);
+		$('#hashnineteen').append(hashtag[18][0]);
+		$('#hashtwenty').append(hashtag[19][0]);		
+		}
 
+// '<span style="margin-left:10px;margin-right:20px;">'+item[i]+'</span> ';
+	
 } 
-	function LoadFunction()
-	{
-		$("#hot_one").html('正在努力的加载中....');
-	}
-
-	function erryFunction()
-	{
-		alert("error");
-
-	}
-
+  //   function randomColor( )
+  //   {  
+  //   	var rand = Math.floor(Math.random( ) * 0xFFFFFF).toString(16);  
+  //   	if(rand.length == 6)
+	 //    {  
+	 //        return rand;  
+		// }
+		// else
+		// {  
+  //       	return randomColor();
+  //       }  
+  //   }
+	
 	//0:uid  1:media  2:photo_url  3:text 4:情绪 5:time  6:gro  
 	//7：文本类型（1为原创微博）8:转发  9:评论 10：情绪 
 	//11：时间戳  12：敏感度 13：mid
@@ -98,3 +142,4 @@ function get_weibo_hashtag(){
 
 
 get_hot_text();
+get_weibo_hashtag();
