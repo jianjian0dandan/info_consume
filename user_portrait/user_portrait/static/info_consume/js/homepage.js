950f0803c35f5492f3571d468c050d1f6e8b3306
linhaobuaa <1257819385@qq.com>
2016-08-31 10:49:58 +0800

    lcr

--- user_portrait/user_portrait/static/info_consume/js/homepage.js
+++ user_portrait/user_portrait/static/info_consume/js/homepage.js
@@ -108,7 +108,7 @@ homepageinfo.prototype = {
         console.log(2);
       } 
    },
-
+   //影响力和活跃度数据
     overallData:function(data){
     console.log(data);
     //影响力
@@ -136,8 +136,18 @@ homepageinfo.prototype = {
       }
      
     }
+  //微博数据处理
+    weiboData:function(data){;
+    	DrawWeibo(data,'group_influ_weibo', 'group_influ_weibo_result');
+        $('#per_onload').css('display','none');
+        $('#group_influ_weibo').css('display', 'block')
+    }  
+
   }
-var uid = 1640601392;
+
+
+
+var uid = 2625203751;
 var Personal = new homepageinfo();
 var personalData; // global data
 
@@ -150,3 +160,19 @@ function openurl(){
   var ourl = $('#openurl').text();
   window.open(ourl);
  }
+
+$(function(){
+	$('#modechoose').click(function(){
+	var box = document.getElementsByName('mode_choose');
+	for(var i=0;i<box.length;i++){
+		if(box[i].checked){
+			sort_type = box[i].value;
+		}
+    }
+$('#per_onload').css('display', 'block');
+$('#group_influ_weibo').css('display', 'none')
+	var url = "/attribute/new_user_weibo/?uid="+uid+"&sort_type="+sort_type;
+	//console.log('ddd',url);
+        Personal.call_sync_ajax_request(url, Personal.ajax_method, Personal.weiboData);
+	});
+});
