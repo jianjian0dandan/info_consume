8792315f71b0199f79c389126b6dff367ddba49a
linhaobuaa <1257819385@qq.com>
2016-09-06 16:33:59 +0800

    9033

--- user_portrait/user_portrait/static/info_consume/js/circle_social.js
+++ user_portrait/user_portrait/static/info_consume/js/circle_social.js
@@ -1,4 +1,39 @@
-function Draw_retwwie_graph(data){
+function Draw_retwwie_in(data){
+var nodes_from = {};
+var nodes_to = {};
+var links_single = {};
+var nodes_total = [];
+var links_total = [];
+if(data.length==0){
+ $('#in-retwwie').empty();
+ var html = '<span style="margin-left:300px;font-size:20px;">暂无数据</span>'; 
+ $('#in-retwwie').append(html);
+}else{
+  for (var i=0;i<data.length;i++){
+  var s = i.toString();
+  nodes_from['category'] = 0;
+  if(data[s]['4']=='unknown'||data[s]['4']==''){
+     nodes_from['name'] = data[s]['1'];
+ }else{
+     nodes_from['name'] = data[s]['4'];
+  };
+   nodes_from['value'] = 10;
+   nodes_total.push(nodes_from);
+
+  nodes_to['category'] = 1;
+  if(data[s]['3']=='unknown'||data[s]['3']==''){
+     nodes_to['name'] = data[s]['0'];
+ }else{
+     nodes_to['name'] = data[s]['3'];
+  };
+   nodes_to['value'] = 10;
+   nodes_total.push(nodes_to);
+
+   links_single['source'] = nodes_from['name'];
+   links_single['target'] = nodes_to['name'];
+   links_single['weight'] = data[s]['2'];
+   links_total.push(links_single);
+  }
 var myChart = echarts.init(document.getElementById('in-retwwie'),'shine');
 option = {
 
@@ -16,22 +51,126 @@ option = {
     },
     legend: {
         x: 'left',
-        data:['家人','朋友']
+        data:['转发用户','被转发用户']
     },
     series : [
         {
             type:'force',
-            name : "人物关系",
+            name : "群组内部转发关系",
             ribbonType: false,
             categories : [
                 {
-                    name: '人物'
+                    name: '群组用户'
+                }
+            ],
+            itemStyle: {
+                normal: {
+                    label: {
+                        show: true,
+                        textStyle: {
+                            color: '#333'
+                        }
+                    },
+                    nodeStyle : {
+                        brushType : 'both',
+                        borderColor : 'rgba(255,215,0,0.4)',
+                        borderWidth : 1
+                    },
+                    linkStyle: {
+                        type: 'curve'
+                    }
                 },
+                emphasis: {
+                    label: {
+                        show: false
+                        // textStyle: null      // 默认使用全局文本样式，详见TEXTSTYLE
+                    },
+                    nodeStyle : {
+                        //r: 30
+                    },
+                    linkStyle : {}
+                }
+            },
+            useWorker: false,
+            minRadius : 15,
+            maxRadius : 25,
+            symbolSize : 10,
+            gravity: 1.1,
+            scaling: 1.1,
+            linkSymbol: 'arrow',
+            roam: 'move',
+            nodes: nodes_total,
+            links: links_total
+        }
+    ]
+};
+ myChart.setOption(option);
+}
+} 
+
+function Draw_retwwie_out(data,div_name){
+var nodes_from = {};
+var nodes_to = {};
+var links_single = {};
+var nodes_total = [];
+var links_total = [];
+if(data.length==0){
+ $('#out-retwwie').empty();
+ var html = '<span style="margin-left:300px;font-size:20px;">暂无数据</span>'; 
+ $('#out-retwwie').append(html);
+}else{
+  for (var i=0;i<data.length;i++){
+  var s = i.toString();
+  nodes_from['category'] = 0;
+  if(data[s]['4']=='unknown'||data[s]['4']==''){
+     nodes_from['name'] = data[s]['1'];
+ }else{
+     nodes_from['name'] = data[s]['4'];
+  };
+   nodes_from['value'] = 10;
+   nodes_total.push(nodes_from);
+
+  nodes_to['category'] = 1;
+  if(data[s]['3']=='unknown'||data[s]['3']==''){
+     nodes_to['name'] = data[s]['0'];
+ }else{
+     nodes_to['name'] = data[s]['3'];
+  };
+   nodes_to['value'] = 10;
+   nodes_total.push(nodes_to);
+
+   links_single['source'] = nodes_from['name'];
+   links_single['target'] = nodes_to['name'];
+   links_single['weight'] = data[s]['2'];
+   links_total.push(links_single);
+  }
+var myChart = echarts.init(document.getElementById('out-retwwie'),'shine');
+option = {
+
+    tooltip : {
+        trigger: 'item',
+        formatter: '{a} : {b}'
+    },
+    toolbox: {
+        show : true,
+        feature : {
+            restore : {show: true},
+            magicType: {show: true, type: ['force', 'chord']},
+            saveAsImage : {show: true}
+        }
+    },
+    legend: {
+        x: 'left',
+        data:['群组内部用户','群组外部用户']
+    },
+    series : [
+        {
+            type:'force',
+            name : "群组外部转发关系",
+            ribbonType: false,
+            categories : [
                 {
-                    name: '家人'
-                },
-                {
-                    name:'朋友'
+                    name: '微博用户'
                 }
             ],
             itemStyle: {
@@ -67,52 +206,24 @@ option = {
             maxRadius : 25,
             gravity: 1.1,
             scaling: 1.1,
+            linkSymbol: 'arrow',
             roam: 'move',
-            nodes:[
-                {category:0, name: '乔布斯', value : 10, label: '乔布斯\n（主要）'},
-                {category:1, name: '丽萨-乔布斯',value : 2},
-                {category:1, name: '保罗-乔布斯',value : 3},
-                {category:1, name: '克拉拉-乔布斯',value : 3},
-                {category:1, name: '劳伦-鲍威尔',value : 7},
-                {category:2, name: '史蒂夫-沃兹尼艾克',value : 5},
-                {category:2, name: '奥巴马',value : 8},
-                {category:2, name: '比尔-盖茨',value : 9},
-                {category:2, name: '乔纳森-艾夫',value : 4},
-                {category:2, name: '蒂姆-库克',value : 4},
-                {category:2, name: '龙-韦恩',value : 1},
-            ],
-            links : [
-                {source : '丽萨-乔布斯', target : '乔布斯', weight : 1, name: '女儿'},
-                {source : '保罗-乔布斯', target : '乔布斯', weight : 2, name: '父亲'},
-                {source : '克拉拉-乔布斯', target : '乔布斯', weight : 1, name: '母亲'},
-                {source : '劳伦-鲍威尔', target : '乔布斯', weight : 2},
-                {source : '史蒂夫-沃兹尼艾克', target : '乔布斯', weight : 3, name: '合伙人'},
-                {source : '奥巴马', target : '乔布斯', weight : 1},
-                {source : '比尔-盖茨', target : '乔布斯', weight : 6, name: '竞争对手'},
-                {source : '乔纳森-艾夫', target : '乔布斯', weight : 1, name: '爱将'},
-                {source : '蒂姆-库克', target : '乔布斯', weight : 1},
-                {source : '龙-韦恩', target : '乔布斯', weight : 1},
-                {source : '克拉拉-乔布斯', target : '保罗-乔布斯', weight : 1},
-                {source : '奥巴马', target : '保罗-乔布斯', weight : 1},
-                {source : '奥巴马', target : '克拉拉-乔布斯', weight : 1},
-                {source : '奥巴马', target : '劳伦-鲍威尔', weight : 1},
-                {source : '奥巴马', target : '史蒂夫-沃兹尼艾克', weight : 1},
-                {source : '比尔-盖茨', target : '奥巴马', weight : 6},
-                {source : '比尔-盖茨', target : '克拉拉-乔布斯', weight : 1},
-                {source : '蒂姆-库克', target : '奥巴马', weight : 1}
-            ]
+            nodes: nodes_total,
+            links: links_total
         }
     ]
 };
-     
  myChart.setOption(option);
+}
 } 
+
 function Draw_retwwie_weibo(data){
 
 
 } 
 function Draw_social_page(data){
-       Draw_retwwie_graph(data);
+       Draw_retwwie_in(data.social_in_record);
+       Draw_retwwie_out(data.social_out_record);
        Draw_retwwie_weibo(data);
 } 
 
