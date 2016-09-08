219a6a8cfebaf45b96f9e34a0350aa65588d6a0c
linhaobuaa <1257819385@qq.com>
2016-09-08 17:47:17 +0800

    yuyuqi

--- user_portrait/user_portrait/static/info_consume/js/my_friend.js
+++ user_portrait/user_portrait/static/info_consume/js/my_friend.js
@@ -339,6 +339,11 @@ my_friend.prototype =
         console.log(data);
         var myChart = echarts.init(document.getElementById('transmit'));
         option = {
+        title : {
+        text: '转发关系网络',
+        x:'right',
+        y:'bottom'
+   		},
         tooltip : {
             trigger: 'item',
             formatter: '{a} : {b}'
@@ -441,12 +446,359 @@ my_friend.prototype =
             }
         ]
     };
-
-		 myChart.setOption(option);
-
-       //$('#p_so_onload').css('display','none').siblings().css('display','block');  
+	   myChart.setOption(option);
+       $('#p_so_onload').css('display','none').siblings().css('display','block');  
 },
 
+	mention_relationship:function(data)
+	{
+		console.log(data);
+        var myChart = echarts.init(document.getElementById('mention'));
+        option = {
+        title : {
+        text: '提及关系网络',
+        x:'right',
+        y:'bottom'
+   		},
+        tooltip : {
+            trigger: 'item',
+            formatter: '{a} : {b}'
+        },
+        toolbox: {
+            show : true,
+            feature : {
+                restore : {show: true},
+                magicType: {show: true, type: ['force', 'chord']},
+                saveAsImage : {show: true}
+            }
+        },
+        legend: {
+            x: 'left',
+            data:['家人','朋友']
+        },
+        series : [
+            {
+                type:'force',
+                name : "人物关系",
+                ribbonType: false,
+                categories : [
+                    {
+                        name: '人物'
+                    },
+                    {
+                        name: '家人'
+                    },
+                    {
+                        name:'朋友'
+                    }
+                ],
+                itemStyle: {
+                    normal: {
+                        label: {
+                            show: true,
+                            textStyle: {
+                                color: '#333'
+                            }
+                        },
+                        nodeStyle : {
+                            brushType : 'both',
+                            borderColor : 'rgba(255,215,0,0.4)',
+                            borderWidth : 1
+                        },
+                        linkStyle: {
+                            type: 'curve'
+                        }
+                    },
+                    emphasis: {
+                        label: {
+                            show: false
+                            // textStyle: null      // 默认使用全局文本样式，详见TEXTSTYLE
+                        },
+                        nodeStyle : {
+                            //r: 30
+                        },
+                        linkStyle : {}
+                    }
+                },
+                useWorker: false,
+                minRadius : 15,
+                maxRadius : 25,
+                gravity: 1.1,
+                scaling: 1.1,
+                roam: 'move',
+                nodes:[
+                    {category:0, name: '乔布斯', value : 10, label: '乔布斯\n（主要）'},
+                    {category:1, name: '丽萨-乔布斯',value : 2},
+                    {category:1, name: '保罗-乔布斯',value : 3},
+                    {category:1, name: '克拉拉-乔布斯',value : 3},
+                    {category:1, name: '劳伦-鲍威尔',value : 7},
+                    {category:2, name: '史蒂夫-沃兹尼艾克',value : 5},
+                    {category:2, name: '奥巴马',value : 8},
+                    {category:2, name: '比尔-盖茨',value : 9},
+                    {category:2, name: '乔纳森-艾夫',value : 4},
+                    {category:2, name: '蒂姆-库克',value : 4},
+                    {category:2, name: '龙-韦恩',value : 1},
+                ],
+                links : [
+                    {source : '丽萨-乔布斯', target : '乔布斯', weight : 1, name: '女儿'},
+                    {source : '保罗-乔布斯', target : '乔布斯', weight : 2, name: '父亲'},
+                    {source : '克拉拉-乔布斯', target : '乔布斯', weight : 1, name: '母亲'},
+                    {source : '劳伦-鲍威尔', target : '乔布斯', weight : 2},
+                    {source : '史蒂夫-沃兹尼艾克', target : '乔布斯', weight : 3, name: '合伙人'},
+                    {source : '奥巴马', target : '乔布斯', weight : 1},
+                    {source : '比尔-盖茨', target : '乔布斯', weight : 6, name: '竞争对手'},
+                    {source : '乔纳森-艾夫', target : '乔布斯', weight : 1, name: '爱将'},
+                    {source : '蒂姆-库克', target : '乔布斯', weight : 1},
+                    {source : '龙-韦恩', target : '乔布斯', weight : 1},
+                    {source : '克拉拉-乔布斯', target : '保罗-乔布斯', weight : 1},
+                    {source : '奥巴马', target : '保罗-乔布斯', weight : 1},
+                    {source : '奥巴马', target : '克拉拉-乔布斯', weight : 1},
+                    {source : '奥巴马', target : '劳伦-鲍威尔', weight : 1},
+                    {source : '奥巴马', target : '史蒂夫-沃兹尼艾克', weight : 1},
+                    {source : '比尔-盖茨', target : '奥巴马', weight : 6},
+                    {source : '比尔-盖茨', target : '克拉拉-乔布斯', weight : 1},
+                    {source : '蒂姆-库克', target : '奥巴马', weight : 1}
+                ]
+            }
+        ]
+    };
+	   myChart.setOption(option);
+       $('#p_so_onload').css('display','none').siblings().css('display','block'); 
+	},
+
+
+	comment_relationship:function(data)
+	{
+		console.log(data);
+        var myChart = echarts.init(document.getElementById('comment'));
+        option = {
+        title : {
+        text: '评论关系网络',
+        x:'right',
+        y:'bottom'
+   		},
+        tooltip : {
+            trigger: 'item',
+            formatter: '{a} : {b}'
+        },
+        toolbox: {
+            show : true,
+            feature : {
+                restore : {show: true},
+                magicType: {show: true, type: ['force', 'chord']},
+                saveAsImage : {show: true}
+            }
+        },
+        legend: {
+            x: 'left',
+            data:['家人','朋友']
+        },
+        series : [
+            {
+                type:'force',
+                name : "人物关系",
+                ribbonType: false,
+                categories : [
+                    {
+                        name: '人物'
+                    },
+                    {
+                        name: '家人'
+                    },
+                    {
+                        name:'朋友'
+                    }
+                ],
+                itemStyle: {
+                    normal: {
+                        label: {
+                            show: true,
+                            textStyle: {
+                                color: '#333'
+                            }
+                        },
+                        nodeStyle : {
+                            brushType : 'both',
+                            borderColor : 'rgba(255,215,0,0.4)',
+                            borderWidth : 1
+                        },
+                        linkStyle: {
+                            type: 'curve'
+                        }
+                    },
+                    emphasis: {
+                        label: {
+                            show: false
+                            // textStyle: null      // 默认使用全局文本样式，详见TEXTSTYLE
+                        },
+                        nodeStyle : {
+                            //r: 30
+                        },
+                        linkStyle : {}
+                    }
+                },
+                useWorker: false,
+                minRadius : 15,
+                maxRadius : 25,
+                gravity: 1.1,
+                scaling: 1.1,
+                roam: 'move',
+                nodes:[
+                    {category:0, name: '乔布斯', value : 10, label: '乔布斯\n（主要）'},
+                    {category:1, name: '丽萨-乔布斯',value : 2},
+                    {category:1, name: '保罗-乔布斯',value : 3},
+                    {category:1, name: '克拉拉-乔布斯',value : 3},
+                    {category:1, name: '劳伦-鲍威尔',value : 7},
+                    {category:2, name: '史蒂夫-沃兹尼艾克',value : 5},
+                    {category:2, name: '奥巴马',value : 8},
+                    {category:2, name: '比尔-盖茨',value : 9},
+                    {category:2, name: '乔纳森-艾夫',value : 4},
+                    {category:2, name: '蒂姆-库克',value : 4},
+                    {category:2, name: '龙-韦恩',value : 1},
+                ],
+                links : [
+                    {source : '丽萨-乔布斯', target : '乔布斯', weight : 1, name: '女儿'},
+                    {source : '保罗-乔布斯', target : '乔布斯', weight : 2, name: '父亲'},
+                    {source : '克拉拉-乔布斯', target : '乔布斯', weight : 1, name: '母亲'},
+                    {source : '劳伦-鲍威尔', target : '乔布斯', weight : 2},
+                    {source : '史蒂夫-沃兹尼艾克', target : '乔布斯', weight : 3, name: '合伙人'},
+                    {source : '奥巴马', target : '乔布斯', weight : 1},
+                    {source : '比尔-盖茨', target : '乔布斯', weight : 6, name: '竞争对手'},
+                    {source : '乔纳森-艾夫', target : '乔布斯', weight : 1, name: '爱将'},
+                    {source : '蒂姆-库克', target : '乔布斯', weight : 1},
+                    {source : '龙-韦恩', target : '乔布斯', weight : 1},
+                    {source : '克拉拉-乔布斯', target : '保罗-乔布斯', weight : 1},
+                    {source : '奥巴马', target : '保罗-乔布斯', weight : 1},
+                    {source : '奥巴马', target : '克拉拉-乔布斯', weight : 1},
+                    {source : '奥巴马', target : '劳伦-鲍威尔', weight : 1},
+                    {source : '奥巴马', target : '史蒂夫-沃兹尼艾克', weight : 1},
+                    {source : '比尔-盖茨', target : '奥巴马', weight : 6},
+                    {source : '比尔-盖茨', target : '克拉拉-乔布斯', weight : 1},
+                    {source : '蒂姆-库克', target : '奥巴马', weight : 1}
+                ]
+            }
+        ]
+    };
+	   myChart.setOption(option);
+       $('#p_so_onload').css('display','none').siblings().css('display','block'); 
+	},
+
+
+	interaction_relationship:function(data)
+	{
+		console.log(data);
+        var myChart = echarts.init(document.getElementById('interaction'));
+        option = {
+        title : {
+        text: '交互关系网络',
+        x:'right',
+        y:'bottom'
+   		},
+        tooltip : {
+            trigger: 'item',
+            formatter: '{a} : {b}'
+        },
+        toolbox: {
+            show : true,
+            feature : {
+                restore : {show: true},
+                magicType: {show: true, type: ['force', 'chord']},
+                saveAsImage : {show: true}
+            }
+        },
+        legend: {
+            x: 'left',
+            data:['家人','朋友']
+        },
+        series : [
+            {
+                type:'force',
+                name : "人物关系",
+                ribbonType: false,
+                categories : [
+                    {
+                        name: '人物'
+                    },
+                    {
+                        name: '家人'
+                    },
+                    {
+                        name:'朋友'
+                    }
+                ],
+                itemStyle: {
+                    normal: {
+                        label: {
+                            show: true,
+                            textStyle: {
+                                color: '#333'
+                            }
+                        },
+                        nodeStyle : {
+                            brushType : 'both',
+                            borderColor : 'rgba(255,215,0,0.4)',
+                            borderWidth : 1
+                        },
+                        linkStyle: {
+                            type: 'curve'
+                        }
+                    },
+                    emphasis: {
+                        label: {
+                            show: false
+                            // textStyle: null      // 默认使用全局文本样式，详见TEXTSTYLE
+                        },
+                        nodeStyle : {
+                            //r: 30
+                        },
+                        linkStyle : {}
+                    }
+                },
+                useWorker: false,
+                minRadius : 15,
+                maxRadius : 25,
+                gravity: 1.1,
+                scaling: 1.1,
+                roam: 'move',
+                nodes:[
+                    {category:0, name: '乔布斯', value : 10, label: '乔布斯\n（主要）'},
+                    {category:1, name: '丽萨-乔布斯',value : 2},
+                    {category:1, name: '保罗-乔布斯',value : 3},
+                    {category:1, name: '克拉拉-乔布斯',value : 3},
+                    {category:1, name: '劳伦-鲍威尔',value : 7},
+                    {category:2, name: '史蒂夫-沃兹尼艾克',value : 5},
+                    {category:2, name: '奥巴马',value : 8},
+                    {category:2, name: '比尔-盖茨',value : 9},
+                    {category:2, name: '乔纳森-艾夫',value : 4},
+                    {category:2, name: '蒂姆-库克',value : 4},
+                    {category:2, name: '龙-韦恩',value : 1},
+                ],
+                links : [
+                    {source : '丽萨-乔布斯', target : '乔布斯', weight : 1, name: '女儿'},
+                    {source : '保罗-乔布斯', target : '乔布斯', weight : 2, name: '父亲'},
+                    {source : '克拉拉-乔布斯', target : '乔布斯', weight : 1, name: '母亲'},
+                    {source : '劳伦-鲍威尔', target : '乔布斯', weight : 2},
+                    {source : '史蒂夫-沃兹尼艾克', target : '乔布斯', weight : 3, name: '合伙人'},
+                    {source : '奥巴马', target : '乔布斯', weight : 1},
+                    {source : '比尔-盖茨', target : '乔布斯', weight : 6, name: '竞争对手'},
+                    {source : '乔纳森-艾夫', target : '乔布斯', weight : 1, name: '爱将'},
+                    {source : '蒂姆-库克', target : '乔布斯', weight : 1},
+                    {source : '龙-韦恩', target : '乔布斯', weight : 1},
+                    {source : '克拉拉-乔布斯', target : '保罗-乔布斯', weight : 1},
+                    {source : '奥巴马', target : '保罗-乔布斯', weight : 1},
+                    {source : '奥巴马', target : '克拉拉-乔布斯', weight : 1},
+                    {source : '奥巴马', target : '劳伦-鲍威尔', weight : 1},
+                    {source : '奥巴马', target : '史蒂夫-沃兹尼艾克', weight : 1},
+                    {source : '比尔-盖茨', target : '奥巴马', weight : 6},
+                    {source : '比尔-盖茨', target : '克拉拉-乔布斯', weight : 1},
+                    {source : '蒂姆-库克', target : '奥巴马', weight : 1}
+                ]
+            }
+        ]
+    };
+	   myChart.setOption(option);
+       $('#p_so_onload').css('display','none').siblings().css('display','block'); 
+	}
 }
 
 var my_friend=new my_friend();
@@ -471,4 +823,15 @@ my_friend.call_sync_ajax_request(url, my_friend.ajax_method, my_friend.intimacy_
 var uid_transmit=2029036025;
 var url ='/info_person_social/follower/?uid='+uid_transmit;
 my_friend.call_sync_ajax_request(url, my_friend.ajax_method, my_friend.transmit_relationship);
-
+//@的用户关系网络
+var uid_mention=1831090244;
+var url ='/info_person_social/mention/?uid='+uid_mention;
+my_friend.call_sync_ajax_request(url, my_friend.ajax_method, my_friend.mention_relationship);
+//评论关系网络图
+var uid_comment=1831090244;
+var url ='/info_person_social/be_comment/?uid='+uid_comment;
+my_friend.call_sync_ajax_request(url, my_friend.ajax_method, my_friend.comment_relationship);
+//交互关系网络图
+var uid_interaction=1831090244;
+var url ='/info_person_social/bidirect_interaction/?uid='+uid_interaction;
+my_friend.call_sync_ajax_request(url, my_friend.ajax_method, my_friend.interaction_relationship);
