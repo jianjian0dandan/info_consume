bd85ea34adcc058eaed035153f8efc88e8024da6
linhaobuaa <1257819385@qq.com>
2016-09-27 10:34:23 +0800

    lcr

--- user_portrait/user_portrait/static/info_consume/js/viewinfo_weibo.js
+++ user_portrait/user_portrait/static/info_consume/js/viewinfo_weibo.js
@@ -1,8 +1,7 @@
-
-function homepageinfo() {
+function viewinfo_weibo() {
 	this.ajax_method = 'GET';
 }
-homepageinfo.prototype = {
+viewinfo_weibo.prototype = {
   call_sync_ajax_request:function(url,method,callback) {
 	  $.ajax({
 		url: url,
@@ -16,34 +15,34 @@ homepageinfo.prototype = {
 
   //console.log(data);
   personalData = data ;
-  var uid_div = document.getElementById('uid');
-  if(personalData.uid){
-      //console.log(personalData.uid)
-      uid_div.innerHTML = personalData.uid;
-  }else{
-      uid_div.innerHTML = "无此数据";
-  }
-  var img = document.getElementById('portraitImg');
-  if(personalData.photo_url == "unknown"){
-      img.src =  "http://tp2.sinaimg.cn/1878376757/50/0/1";
-  }else{
-      img.src = personalData.photo_url;
-  }
+  // var uid_div = document.getElementById('uid');
+  // if(personalData.uid){
+  //     //console.log(personalData.uid)
+  //     uid_div.innerHTML = personalData.uid;
+  // }else{
+  //     uid_div.innerHTML = "无此数据";
+  // }
+  // var img = document.getElementById('portraitImg');
+  // if(personalData.photo_url == "unknown"){
+  //     img.src =  "http://tp2.sinaimg.cn/1878376757/50/0/1";
+  // }else{
+  //     img.src = personalData.photo_url;
+  // }
 
-  var nickName = document.getElementById('username');
-  if(personalData.nick_name){
-      if (personalData.nick_name == 'unknown') {
-          nickName.innerHTML = '未知';
-      }
-      else{
-          nickName.innerHTML = personalData.nick_name;
-      }
-  }else{
-      nickName.innerHTML = "无此数据";
-  }
+  // var nickName = document.getElementById('username');
+  // if(personalData.nick_name){
+  //     if (personalData.nick_name == 'unknown') {
+  //         nickName.innerHTML = '未知';
+  //     }
+  //     else{
+  //         nickName.innerHTML = personalData.nick_name;
+  //     }
+  // }else{
+  //     nickName.innerHTML = "无此数据";
+  // }
    
-   var nickName_greet = document.getElementById('username_greet');
-     nickName_greet.innerHTML = personalData.nick_name;
+   // var nickName_greet = document.getElementById('username_greet');
+   //   nickName_greet.innerHTML = personalData.nick_name;
         
    var Verfi = document.getElementById('verified');
    if( personalData.verified_type==""){
@@ -62,17 +61,17 @@ homepageinfo.prototype = {
        Verfi.innerHTML = "是";
      Verf_type.innerHTML = personalData.verified_type_ch;
    }
-       var Fansum = document.getElementById('fansum');
+       var Fansum = document.getElementById('fansumview');
    if( personalData.fansnum==""){
        personalData.fansnum = "暂无数据";
    }
        Fansum.innerHTML = personalData.fansnum;
-   var Attentionsum = document.getElementById('attentionsum');
+   var Attentionsum = document.getElementById('attentionsumview');
    if( personalData.friendsnum==""){
        personalData.friendsnum = "暂无数据";
    }
        Attentionsum.innerHTML = personalData.friendsnum;
-   var Weibosum = document.getElementById('weibosum');
+   var Weibosum = document.getElementById('weibosumview');
    if( personalData.statusnum==""){
        personalData.statusnum = "暂无数据";
    }
@@ -86,70 +85,29 @@ homepageinfo.prototype = {
    if( personalData.description==""){
        personalData.description = "暂无数据";
        }else if (personalData.description.length>50){
-       Descrip.innerHTML = personalData.description.substr(0,50)+'...';
+       Descrip.innerHTML = personalData.description.substr(0,4)+'...';
            Descrip.title = personalData.description;
       }else{
        Descrip.innerHTML = personalData.description;}
-           var homepage = document.getElementById('uhome');
-           if(!personalData.uid ){
-               homepage.innerHTML = "无此数据";
-           }else{
-               homepage.innerHTML = '<a id="openurl" style="cursor:pointer" onclick="openurl();">http://weibo.com/u/'+personalData.uid;
-           }  
-    var userImg = document.getElementById('body_board')
-      //console.log(userImg);
-      if (personalData.sex == ""){
-  	    personalData.sex = '暂无数据';
-  	    //console.log(none);	
-      } else if (personalData.sex == 1) {
-        userImg.src = "/static/info_consume/image/bodymodel_man.png";
-      //  console.log(1);
-      }else if (personalData.sex == 2) {
-        userImg.src = "/static/info_consume/image/bodymodel_woman.png";
-        userImg.style = "width: 350px;margin-left: -77px;float: none;margin-top: 60px;display: block;opacity:0.8;";
-       // console.log(2);
-      } 
-   
-   },
-   //影响力和活跃度数据
-    overallData:function(data){
-    //console.log(data);
-    //影响力
-     var FP = document.getElementById('influencePower');
-       if(data.influence[0] !=""){
-           FP.innerHTML = data.influence[0].toFixed(2);
-       }else{
-           FP.innerHTML = '0';
-       }
-      var perofinf = data.influence[0];
-      perofinf = perofinf/100*440;
-      $("#percentBar").css("width",perofinf);
-    //活跃度
-     var AP = document.getElementById('activePower');
-       if(data.activeness[0] !=""){
-          AP.innerHTML = data.activeness[0].toFixed(2);
-       }else{
-          AP.innerHTML = '0'
-       }
-     var actScore = document.getElementById('act_text');
-     	if (data.activeness[0]<80) {
-        actScore.innerHTML = '最近活跃度有所下降哦，宝宝不开心了￣へ￣';
-      }else if (data.activeness[0]>=80){
-        actScore.innerHTML = '最近很活跃，么么哒(づ￣ 3￣)づ';
-      }
-    },
+           // var homepage = document.getElementById('uhome');
+           // if(!personalData.uid ){
+           //     homepage.innerHTML = "无此数据";
+           // }else{
+           //     homepage.innerHTML = '<a id="openurl" style="cursor:pointer" onclick="openurl();">http://weibo.com/u/'+personalData.uid;
+           // }  
 
-    // 微博数据
-    weiboData:function(data){
-      weiboData = data;
-    	DrawWeibo(data,'group_influ_weibo', 'group_influ_weibo_result');
-        $('#per_onload').css('display','none');
-        $('#group_influ_weibo').css('display', 'block');
-    }
+   },
+   
+   weiboData:function(data){
+     weiboData = data;
+   	DrawWeibo(data,'group_influ_weibo', 'group_influ_weibo_result');
+       $('#per_onload').css('display','none');
+       $('#group_influ_weibo').css('display', 'block');
+   }
 }
-var Personal = new homepageinfo();
+
+var Personal = new viewinfo_weibo();
 var personalData; // global data
-var overallData;
 var weiboData;
 var uid = 1640601392;
 
@@ -366,10 +324,10 @@ function page_group_influ_weibo(start_row,end_row,data, sub_div_name){
         var tweet_ulr = data[i][10];
         //date = new Date(parseInt(timestamp)*1000).format("yyyy-MM-dd hh:mm:ss");
         if (i%2 ==0){
-            html += '<div style="float:left;padding:5px;width: 100%;background:whitesmoke;">';
+            html += '<div style="float:left;padding:5px;width: 95%;background:whitesmoke;">';
     }
         else{
-            html += '<div style="float:left;padding:5px;width: 100%;">';
+            html += '<div style="float:left;padding:5px;width: 95%;">';
     }
     if(geo==null){
         geo='未知';
@@ -387,16 +345,8 @@ function page_group_influ_weibo(start_row,end_row,data, sub_div_name){
     $('#'+sub_div_name).append(html);
 }
 
-var uid = 1640601392;
-var Personal = new homepageinfo();
-var personalData; // global data
-
-
 var url = "/attribute/new_user_profile/?uid=" + uid;
 Personal.call_sync_ajax_request(url, Personal.ajax_method, Personal.personData);
-var url = "/attribute/new_user_evaluate/?uid=" + uid;
-Personal.call_sync_ajax_request(url, Personal.ajax_method, Personal.overallData);
+
 var url = "/attribute/new_user_weibo/?uid="+uid+"&sort_type=timestamp";
 Personal.call_sync_ajax_request(url, Personal.ajax_method, Personal.weiboData);
-
-//最新update
