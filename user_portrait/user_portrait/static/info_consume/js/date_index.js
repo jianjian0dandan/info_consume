2fb4987e36d4e413d1c177946a2ee7eb2b2ef92d
linhaobuaa <1257819385@qq.com>
2016-10-19 22:01:49 +0800

    yuanhuiru

--- user_portrait/user_portrait/static/info_consume/js/date_index.js
+++ user_portrait/user_portrait/static/info_consume/js/date_index.js
@@ -2,9 +2,10 @@ var topics = [];
 var starts_time = [];
 var ends_time = [];
 
-var topic_name='';
-var date_from='';
-var date_to='';
+
+
+
+
 
 
 
@@ -46,15 +47,16 @@ function datetime_to_timestamp(datetime) {
      return timestamp;
  }
 
-function check_status(){
-
+function submit_topic(){
+  Draw_similar_topic_result();
+  Submit_task_result();
 }
 
-
-function go_to_datail(){
+function go_to_datail(topic_name,date_from,date_to){
 
     window.open('/topic_time_analyze/time/?topic_name='+topic_name+'&date_from='+date_from+'&date_to='+date_to);
 }
+
 
 
 function topic_analysis_index(){
@@ -74,8 +76,11 @@ topic_analysis_index.prototype = {   //获取数据，重新画表
 
   Submit_task:function(data){
       // var item = data;
-      console.log('99999');
+      // console.log('99999');
       console.log(data);
+      if(data='success'){
+        alert('任务提交成功！');
+      }
 
   },
 
@@ -104,16 +109,23 @@ topic_analysis_index.prototype = {   //获取数据，重新画表
       html += '<td>'+start_time+'</td>';
       html += '<td>'+end_time+'</td>';
       html += '<td>'+compute_status+'</td>';
-      html += '<td><a href="" onclick="go_to_datail()">'+'查看详情'+'</a></td>';
+      var topic_name=data['recommend'][key][0][0];
+      var date_from=data['recommend'][key][0][1];
+      var date_to=data['recommend'][key][0][2];
+      html += '<td><a href="/topic_time_analyze/time/?topic_name='+topic_name+'&date_from='+date_from+'&date_to='+date_to+'" target="_blank">'+'查看详情'+'</a></td>';
       html += '<tr>';
     }
     html += '</tbody></table></center>';
-    html += '<button type="button" class="btn btn-success" style="margin-left: 70%;" onclick="">创建我的话题任务</button>';
+    // html += '<button type="button" class="btn btn-success" style="margin-left: 70%;" data-toggle="modal" data-target="#create_task">创建我的话题任务</button>';
     $('#index_bottom').append(html);
 
   },
 
   Draw_similar_topic:function(data){
+
+    var create_topic_name=$("#create_topic").val();
+    var create_date_from=$("#input_date_from_create").val();
+    var create_date_to=$("#input_date_to_create").val();
     $('#index_bottom').empty();
     var html='';
     html += '<center>';
@@ -121,7 +133,20 @@ topic_analysis_index.prototype = {   //获取数据，重新画表
     html += '<caption style="color: #00CC66;font-size: 28px;font-family: Microsoft YaHei;text-align:center;">相似话题推荐</caption>';
     html += '<thead><tr><th>话题名称</th><th>开始时间</th><th>终止时间</th><th>计算状态</th><th>操作</th></tr></thead>';
     html += '<tbody>';
-    for(key in data[key]){
+    if(create_topic_name && create_date_from && create_date_to){
+          html += '<tr>';
+          html += '<td>'+create_topic_name+'</td>';
+          html += '<td>'+create_date_from+'</td>';
+          html += '<td>'+create_date_to+'</td>';
+          html += '<td>'+'正在计算'+'</td>';
+          html += '<td><a href="/topic_time_analyze/time/?topic_name='+create_topic_name+'&date_from='+create_date_from+'&date_to='+create_date_to+'" target="_blank">'+'查看详情'+'</a></td>';
+          html += '<tr>';
+          console.log('333333');
+    }
+
+    for(key in data){
+      console.log(key);
+      console.log(data[key]);
       for(i=0;i<data[key].length;i++){
           html += '<tr>';
           html += '<td>'+data[key][i][0]+'</td>';
@@ -138,60 +163,22 @@ topic_analysis_index.prototype = {   //获取数据，重新画表
           html += '<td>'+start_time+'</td>';
           html += '<td>'+end_time+'</td>';
           html += '<td>'+compute_status+'</td>';
-          html += '<td><a href="" onclick="go_to_datail()">'+'查看详情'+'</a></td>';
+          html += '<td><a href="" onclick="go_to_datail(data[key][i][0],data[key][i][1],data[key][i][2])">'+'查看详情'+'</a></td>';
           html += '<tr>';
       }
     
     }
     html += '</tbody></table></center>';
-
+    html += '<button type="button" class="btn btn-success" style="margin-left: 60%;" onclick="">计算状态刷新</button>';
+    html += '<button type="button" class="btn btn-success" style="margin-left: 70%;margin-top: -4.1%;" data-toggle="modal" data-target="#create_task">创建我的话题任务</button>';
     $('#index_bottom').append(html);
   },
-  
-  Draw_topic_name: function(data){
-  	var item = data;
-  	console.log(item);
-  	var html = '';
 
-   
-  	for(key in item){
-  		topics.push(item[key][0]);
-  		starts_time.push(item[key][1]);
-  		ends_time.push(item[key][2]);
+  Draw_all_topic:function(data){
 
-  	}
-  	console.log(topics);
-    console.log(topics.length);
-    var num_row = 0;
-    html += '<center>';
-  	html += '<table id="table" align="center" style = "margin-bottom:150px;margin-right: auto;margin-left: auto;">';
-  	for(i=0;i<Math.min(4,Math.ceil(topics.length/3));i++){
-  		html += '<tr class="height">';
-  		for(j=num_row;j<(num_row+3);j++){
-        if(topics[j]){
-
-          topic_name=topics[j];
-          date_from = new Date(parseInt(starts_time[j]) * 1000).toLocaleString().replace(/:\d{1,2}$/,' ');
-          date_to = new Date(parseInt(ends_time[j]) * 1000).toLocaleString().replace(/:\d{1,2}$/,' ');
-          // date_from = starts_time[j];
-          // date_to = ends_time[j];
-
-          html += '<td><img class="topic_tag" id="topic_tag" src="../../static/info_consume/image/topic_tag.png"></td>';
-          // html += '<td><p id="topic" class="topic_font"><a href="/topic_time_analyze/time/" onclick="go_to_datail('+topic_name+','+date_from+','+date_to+')">#'+topics[j]+'#</a></p></td>';
-          html += '<td><p id="topic" class="topic_font" onclick="go_to_datail()"><a style="color: #00CC66;">#'+topics[j]+'#<a></p></td>';
-          
-        }
-  		}
-      if(num_row<Math.min(4,Math.ceil(topics.length/3))){
-        num_row = num_row+3;
-      }
-  		html += '</tr>';
-  	}
-	html += '</table>';
-  html += '</center>';
-	$('#index_bottom').append(html);
-  
   },
+  
+  
 
 }
 
@@ -199,36 +186,41 @@ var topic_analysis_index = new topic_analysis_index();
 
 
 function Draw_hot_topic_result(){
+  
   var user = '奥运';
-  url = "/topic_language_analyze/topics/?user"+user;
+  url = "/topic_language_analyze/topics/?user="+user;
   console.log(url);
   topic_analysis_index.call_sync_ajax_request(url,topic_analysis_index.Draw_hot_topic);
 } 
 
 function Draw_similar_topic_result(){
-  var keyword = '奥运';
-  url = "/topic_language_analyze/key_topics/?keywords"+keyword;
+  var keyword = $('#input').val();
+  if($('#create_topic').val()){
+    var keyword = $('#create_topic').val();
+  }
+  
+  url = "/topic_language_analyze/key_topics/?keyword="+keyword;
   console.log(url);
   topic_analysis_index.call_sync_ajax_request(url,topic_analysis_index.Draw_similar_topic);
 } 
  
-
-function Draw_topic_name_result(){
-  url = "/topic_language_analyze/topics/";
- 	console.log(url);
- 	topic_analysis_index.call_sync_ajax_request(url,topic_analysis_index.Draw_topic_name);
-}	
+function Draw_all_topic_result(){
+ 
+  var user = '奥运';
+  url = "/topic_language_analyze/topics/?user="+user;
+  console.log(url);
+  topic_analysis_index.call_sync_ajax_request(url,topic_analysis_index.Draw_all_topic);
+} 
 
 function Submit_task_result(){
-  var topic = '试';
-  var start_ts=1468080000;
-  var end_ts=1468944000;
-  var en_name = 'try';
-  var submit_user = 'admin@qq.com';
-  url = "/topic_language_analyze/submit_task/?topic=" + topic+'&start_ts='+start_ts+'&end_ts='+end_ts+'&en_name='+en_name+'&submit_user='+submit_user;
+  var topic = $("#create_topic").val();
+  var start_ts=datetime_to_timestamp($("#input_date_from_create").val());
+  var end_ts=datetime_to_timestamp($("#input_date_to_create").val());
+  var submit_user = '{{g.user.email}}';
+  url = "/topic_language_analyze/submit_task/?topic=" + topic+'&start_ts='+start_ts+'&end_ts='+end_ts+'&submit_user='+submit_user;
   console.log(url);
   topic_analysis_index.call_sync_ajax_request(url,topic_analysis_index.Submit_task);
-  console.log('888');
+  
 } 
 Draw_hot_topic_result();
 // Draw_topic_name_result();
