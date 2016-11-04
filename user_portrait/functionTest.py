246951c36d401da4d60e71abda983e26b07d3e4f
zengxy <xianyu.z@foxmail.com>
2016-11-04 19:38:25 +0800

    test update

--- user_portrait/functionTest.py
+++ user_portrait/functionTest.py
@@ -5,17 +5,41 @@
 __author__ = 'zxy'
 
 from user_portrait.global_utils import es_flow_text, flow_text_index_name_pre, flow_text_index_type, \
-    es_user_profile, profile_index_name, profile_index_type
+    es_user_profile, profile_index_name, profile_index_type, \
+    es_user_portrait, portrait_index_name, portrait_index_type
 from user_portrait.attribute.influence_appendix import weiboinfo2url
 
+import json
+import pprint
 
-def esTest():
-    uid = 1640601392
-    user_profile_result = es_user_profile.get_source(index=profile_index_name, doc_type=profile_index_type, \
-                                                  id=uid)
+uid = 1640601392
 
+def esUserProfileTest():
+    user_profile_result = es_user_profile.\
+        get_source(index=profile_index_name, doc_type=profile_index_type,id=uid)
 
     print user_profile_result
 
+def esUserPortraitTest():
+    user_portrait_result = es_user_portrait.\
+        get_source(index=portrait_index_name, doc_type=profile_index_type,id=uid)
+
+    keywords_items = sorted(json.loads(user_portrait_result["keywords"]),
+                            key = lambda kw: kw[1],
+                            reverse = True)
+    topic_items = sorted(json.loads(user_portrait_result["topic"]).items(),
+                         key=lambda kw: kw[1],
+                         reverse = True)
+
+    for (k,v) in keywords_items:
+        print k,v
+
+    print "----------------------------"
+
+    for (k,v) in topic_items:
+        print k,v
+
+
+
 if __name__ == '__main__':
-    esTest()
+    esUserPortraitTest()
