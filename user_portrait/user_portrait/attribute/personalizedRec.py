6879d35d496411f973d5d526a669eb7e0b74ce3f
zengxy <xianyu.z@foxmail.com>
2016-11-18 20:01:34 +0800

    添加人物推荐的部分主体框架

--- user_portrait/user_portrait/attribute/personalizedRec.py
+++ user_portrait/user_portrait/attribute/personalizedRec.py
@@ -6,19 +6,20 @@ __author__ = 'zxy'
 import time
 import os
 import codecs
+import json
+import Queue
 
-from user_portrait.time_utils import ts2datetime, datetime2ts, ts2date
+from user_portrait.time_utils import ts2datetime, datetime2ts
 from user_portrait.parameter import RUN_TYPE, RUN_TEST_TIME
-from user_portrait.global_utils import es_flow_text, flow_text_index_name_pre, flow_text_index_type, \
-    es_user_profile, profile_index_name, profile_index_type, \
-    es_user_portrait, portrait_index_name, portrait_index_type, \
-    es_ads_weibo, ads_weibo_index_name, ads_weibo_index_type
-
-from user_portrait.parameter import DAY, MAX_VALUE, HOUR
-
+from user_portrait.global_utils import es_flow_text, flow_text_index_name_pre, es_user_profile, profile_index_name, \
+    profile_index_type, \
+    es_user_portrait, portrait_index_name, ads_weibo_index_type, portrait_index_type
+from user_portrait.parameter import DAY, HOUR
 from ads_classify import adsClassify
 from user_portrait.attribute.influence_appendix import weiboinfo2url
 from user_portrait.zxy_params import ADS_TOPIC_TFIDF_DIR
+from user_portrait.global_config import R_BEGIN_TIME
+from user_portrait.global_utils import retweet_index_name_pre, retweet_index_type, es_retweet
 
 
 def adsRec(uid, queryInterval=HOUR * 4):
@@ -42,10 +43,10 @@ def adsRec(uid, queryInterval=HOUR * 4):
     user_key_words = set(user_portrait_result["keywords_string"].split("&"))
     # keywords_items = sorted(json.loads(user_portrait_result["keywords"]),
     # key=lambda kw: kw[1],
-    #                         reverse=True)
+    # reverse=True)
     # topic_items = sorted(json.loads(user_portrait_result["topic"]).items(),
-    #                      key=lambda kw: kw[1],
-    #                      reverse=True)
+    # key=lambda kw: kw[1],
+    # reverse=True)
 
     # test，目前使用的是从原始数据中读取一定时间段内的微博并实时计算的方式得到
     #
@@ -53,7 +54,7 @@ def adsRec(uid, queryInterval=HOUR * 4):
     ads_weibo_all = es_flow_text.search(index=ads_weibo_index_name,
                                         doc_type=ads_weibo_index_type,
                                         body={'query': {"filtered": {"filter": {
-                                        "range": {"timestamp": {"gte": datetime2ts(now_date) - queryInterval}}}}},
+                                            "range": {"timestamp": {"gte": datetime2ts(now_date) - queryInterval}}}}},
                                               'size': 2000,
                                         }
     )['hits']['hits']
@@ -69,6 +70,7 @@ def adsRec(uid, queryInterval=HOUR * 4):
     return ads_weibo_prefer
 
 
+# 广告分类词的TF-IDF
 def construct_topic_word_weight_dic(topic_word_weight_dir):
     topic_word_weight_dic = dict()
     for file_name in os.listdir(topic_word_weight_dir):
@@ -86,6 +88,7 @@ def construct_topic_word_weight_dic(topic_word_weight_dir):
     return topic_word_weight_dic
 
 
+# user在广告topic上的preference
 def construct_topic_feature_dic(words, topic_word_weight_dic):
     user_prefer_dic = dict()
     for (topic_name, word_weight_dic) in topic_word_weight_dic.items():
@@ -96,6 +99,7 @@ def construct_topic_feature_dic(words, topic_word_weight_dic):
     return user_prefer_dic
 
 
+# 判断广告类别
 def judge_ads_topic(words, topic_word_weight_dic):
     ads_feature_dic = construct_topic_feature_dic(words, topic_word_weight_dic)
     max_topic = None
@@ -107,7 +111,7 @@ def judge_ads_topic(words, topic_word_weight_dic):
     return max_topic
 
 
-def adsPreferred(user_topic_dic, weibo_all, topic_word_weight_dic, k):
+def adsPreferred(user_topic_dic, weibo_all, topic_word_weight_dic, k=30):
     '''
     :param user_topic: 用户的topic偏好
     :param weibo: weibo/ad_weibo列表
@@ -128,7 +132,7 @@ def adsPreferred(user_topic_dic, weibo_all, topic_word_weight_dic, k):
 
     ads_midsPrefered = sorted(ads_midsPrefered.items(), key=lambda ads: ads[1], reverse=True)
 
-    k = min(30, len(ads_midsPrefered))
+    k = min(k, len(ads_midsPrefered))
 
     for midInfo in ads_midsPrefered[:k]:
         mid = midInfo[0]
@@ -139,6 +143,103 @@ def adsPreferred(user_topic_dic, weibo_all, topic_word_weight_dic, k):
         # print midInfo[1], weiboMap[mid]["text"]
 
     return adsPreferList
+
+
+'''
+下面是用户推荐的部分
+'''
+
+
+def personRec(uid):
+    direct_attention_id_set = search_attention_id(uid, 1)
+    # 候选推荐
+    candidate_attention_id_set = set()
+    search_queue = Queue.Queue()
+    for user_id in direct_attention_id_set:
+        search_queue.put(user_id)
+
+    while search_queue.not_empty and candidate_attention_id_set < 200:
+        pass
+
+
+
+    for uid in direct_attention_id_set:
+        candidate_attention_id_set = candidate_attention_id_set | search_attention_id(uid)
+
+    candidate_attention_id_set = candidate_attention_id_set - direct_attention_id_set
+    print len(candidate_attention_id_set)
+
+    candidate_user_portrait = search_user_portrait_by_user_ids(candidate_attention_id_set)
+    print len(candidate_user_portrait)
+    # return candidate_user_profile
+    return [dict()]
+
+def sim_user(uid, candidate_attention_id_set):
+    user_portrait = es_user_portrait. \
+        get_source(index=portrait_index_name, doc_type=profile_index_type, id=uid)
+
+    candidate_attention_user_portraits = search_user_portrait_by_user_ids(candidate_attention_id_set)
+
+
+
+
+
+def search_attention_id(uid, k=30):
+    now_ts = time.time()
+    db_number = get_db_num(now_ts)
+    index_name = retweet_index_name_pre + str(db_number)
+    try:
+        retweet_result = es_retweet.get(index=index_name, doc_type=retweet_index_type, id=uid)['_source']
+    except:
+        return set()
+    if retweet_result:
+        retweet_dict = json.loads(retweet_result['uid_retweet'])
+        return set(retweet_dict.keys())
+
+
+def get_db_num(timestamp):
+    date = ts2datetime(timestamp)
+    date_ts = datetime2ts(date)
+    r_beigin_ts = datetime2ts(R_BEGIN_TIME)
+    db_number = ((date_ts - r_beigin_ts) / (DAY * 7)) % 2 + 1
+    # run_type
+    if RUN_TYPE == 0:
+        db_number = 1
+    return db_number
+
+
+def search_user_portrait_by_user_ids(users):
+    users = list(users)
+    user_portrait_return = dict()
+    try:
+        candidate_attention_user_portraits = es_retweet.mget(index=portrait_index_name,
+                                                             doc_type=portrait_index_type,
+                                                             body={'ids': list(users)})['docs']
+    except:
+        candidate_attention_user_portraits = []
+
+    for out_user_item in candidate_attention_user_portraits:
+        if out_user_item['found']:
+            uid = out_user_item['_id']
+            user_portrait_return[uid] = out_user_item['_source']
+    return user_portrait_return
+
+
+def search_user_profile_by_user_ids(users):
+    users = list(users)
+    user_profile_return = dict()
+    try:
+        user_result = es_user_profile.mget(index=profile_index_name,
+                                           doc_type=profile_index_type,
+                                           body={'ids': users})['docs']
+    except:
+        user_result = []
+
+    for out_user_item in user_result:
+        if out_user_item['found']:
+            uid = out_user_item['_id']
+            user_profile_return[uid] = out_user_item['_source']
+    return user_profile_return
 
 
 if __name__ == '__main__':
